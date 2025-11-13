70abe3f2c13e01361646de94952fe83693e153a1
sanjuramu248-web <sanjuramu248@gmail.com>
2025-11-13 17:50:00 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://roxiler-rate-my-store-project-full-nine.vercel.app/",
     credentials: true
 }))
 
