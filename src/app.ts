112c8821447d3425cb4e1ad700a85ae5a520f923
sanjuramu248-web <sanjuramu248@gmail.com>
2025-11-13 17:34:20 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from "cors"
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173', 'https://roxiler-rate-my-store-project-full-nine.vercel.app/');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
